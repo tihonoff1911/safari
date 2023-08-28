2022965905df89d81da6a27328c54ee403868cdc
tihonoff1911 <106765177+tihonoff1911@users.noreply.github.com>
2023-08-28 14:10:52 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(function(){
+
+    $('.gallery__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+        
+
+    });
+
+
+    
+
+
+    $('.gallery__item-inner').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		
+	});
+
+
+	$('.popup__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+
+	$(".booking__phone").mask("+7(999) 999-99-99");
+
+    
+    
+    
+    
+
+});
